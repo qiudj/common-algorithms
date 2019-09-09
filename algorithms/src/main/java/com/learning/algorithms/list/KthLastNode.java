b25114241c0eb6194730002033684882096c666a
ansel09 <qiudj09@163.com>
2019-09-09 20:48:02 +0800

    add solution for deleting kth last node in a single list

---
+++ algorithms/src/main/java/com/learning/algorithms/list/KthLastNode.java
@@ -0,0 +1,42 @@
+package com.learning.algorithms.list;
+
+/**
+ * 关于单链表的倒数K个结点的问题
+ * @date 2019年9月9日20:36:15
+ */
+public class KthLastNode {
+
+    /*返回删除之后的头节点*/
+    public static Node deleteKthLastNode(Node head, int k){
+        if (k <= 0 || head == null) return head;
+
+        Node first = head, second = head, prev = null;
+        while (first != null && k > 0){
+            first = first.next;
+            k--;
+        }
+        if (k > 0) return head;
+
+        while (first != null){
+            first = first.next;
+            prev = second;
+            second = second.next;
+        }
+
+        if (prev != null){
+            prev.next = second.next;
+            return head;
+        } else { //考虑删除首节点的情况
+            return head.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node head = ListUtils.buildList();
+        ListUtils.printList(head);
+
+        //删除倒数第二个元素
+        Node newHead = deleteKthLastNode(head, 6);
+        ListUtils.printList(newHead);
+    }
+}
--- algorithms/src/main/java/com/learning/algorithms/list/ListUtils.java
+++ algorithms/src/main/java/com/learning/algorithms/list/ListUtils.java
@@ -5,11 +5,27 @@ public class ListUtils {
     * 打印链表 */
     public static void printList(Node root){
         if (root == null)   return;
-        System.out.print("打印链表： ");
+        System.out.println("打印链表： ");
         while (root != null){
-            System.out.println(root.value + " ");
+            System.out.print(root.value + " ");
             root = root.next;
         }
         System.out.println();
+    }
+
+    /* 创建单向链表1->2->3->4->5->6，并返回头结点*/
+    public static Node buildList(){
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        Node node5 = new Node(5);
+        Node node6 = new Node(6);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+        return  node1;
     }
 }
